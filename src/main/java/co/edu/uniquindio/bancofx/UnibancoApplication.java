e14290168ad07cb905bfbf0b1f81c46ceab15f42
Omar Piedrahita <omar.piedrahitat@gmail.com>
2023-02-20 11:23:51 -0500

    Ningún cambio relevante

--- src/main/java/co/edu/uniquindio/bancofx/UnibancoApplication.java
+++ src/main/java/co/edu/uniquindio/bancofx/UnibancoApplication.java
@@ -17,7 +17,5 @@ public class UnibancoApplication extends Application {
         stage.show();
     }
 
-    public static void main(String[] args) {
-        launch();
-    }
+    public static void main(String[] args) {launch();}
 }
