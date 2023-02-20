0a2688d7c60ac3c21b80ea9c4a00d254c07a3b0b
CarlosMancera <carlosh.mancerar@uqvirtual.edu.co>
2023-02-19 20:32:52 -0500

    creación clase GUI transacciones
    Se finaliza la clase de Transacciones con sus respectivos módulos

--- src/main/java/co/edu/uniquindio/bancofx/MenuTransaccionController.java
+++ src/main/java/co/edu/uniquindio/bancofx/MenuTransaccionController.java
@@ -1,4 +1,25 @@
 package co.edu.uniquindio.bancofx;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class MenuTransaccionController {
+
+    @FXML
+    private Button btnListaTransacciones;
+
+    @FXML
+    private Button btnRealizarTransaccion;
+
+    @FXML
+    void abrirListaTransacciones(ActionEvent event) {
+
+    }
+
+    @FXML
+    void realizarTransaccion(ActionEvent event) {
+
+    }
+
 }
