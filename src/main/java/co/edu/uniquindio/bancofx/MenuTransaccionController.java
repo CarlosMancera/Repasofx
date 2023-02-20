5ba2f8ed0b99a6745a732e824eff3739f7de7b8e
Omar Piedrahita <omar.piedrahitat@gmail.com>
2023-02-20 11:57:56 -0500

    Actualizo controlador de menu-transaccion.fxml y corrijo errores en las lineas de xmlns

--- src/main/java/co/edu/uniquindio/bancofx/MenuTransaccionController.java
+++ src/main/java/co/edu/uniquindio/bancofx/MenuTransaccionController.java
@@ -1,4 +1,26 @@
 package co.edu.uniquindio.bancofx;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class MenuTransaccionController {
+
+    @FXML
+    private Button btnListaTrransacciones;
+
+    @FXML
+    private Button btnRealizarTransaccion;
+
+    @FXML
+    void abrirListaTransacciones(ActionEvent event) {
+
+    }
+
+    @FXML
+    void realizarTransaccion(ActionEvent event) {
+
+    }
+
 }
+
