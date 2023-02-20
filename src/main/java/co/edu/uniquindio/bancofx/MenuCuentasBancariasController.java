1b6c1745cdd16eae86f210f98af089fe8ceba152
CarlosMancera <carlosh.mancerar@uqvirtual.edu.co>
2023-02-19 23:31:44 -0500

    Se Agrega Clase cuenta Bancaria
    Campos para cuenta bancaria, CRUD para crear, borrar, eliminar usuarios

---
+++ src/main/java/co/edu/uniquindio/bancofx/MenuCuentasBancariasController.java
@@ -0,0 +1,47 @@
+package co.edu.uniquindio.bancofx;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class MenuCuentasBancariasController {
+
+    @FXML
+    private Button btnAgregarClientes;
+
+    @FXML
+    private ChoiceBox<?> lstTipoCuenta;
+
+    @FXML
+    private TableView<?> tblListaClientes;
+
+    @FXML
+    private TextField txtAgregarNombre;
+
+    @FXML
+    private TextField txtCedula;
+
+    @FXML
+    void abrirListaClientes(ActionEvent event) {
+
+    }
+
+    @FXML
+    void agregarClientes(ActionEvent event) {
+
+    }
+
+    @FXML
+    void nombreCliente(ActionEvent event) {
+
+    }
+
+    @FXML
+    void numeroCedula(ActionEvent event) {
+
+    }
+
+}
