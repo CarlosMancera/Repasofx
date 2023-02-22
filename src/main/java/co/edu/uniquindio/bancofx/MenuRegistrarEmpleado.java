3a69d93f03768098fe78dfa84b3a7832f146de89
CarlosMancera <carlosh.mancerar@uqvirtual.edu.co>
2023-02-21 23:35:44 -0500

    Se crean todas las GUI,
    Para finalizar se creó la GUI de registrar empleado que estaba haciendo falta, con todos los campos completos

--- src/main/java/co/edu/uniquindio/bancofx/MenuRegistrarEmpleado.java
+++
@@ -1,4 +0,0 @@
-package co.edu.uniquindio.bancofx;
-
-public class MenuRegistrarEmpleado {
-}
---
+++ src/main/java/co/edu/uniquindio/bancofx/MenuRegistrarEmpleadoController.java
@@ -0,0 +1,76 @@
+package co.edu.uniquindio.bancofx;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class MenuRegistrarEmpleadoController {
+
+    @FXML
+    private Button btnListaEmpleados;
+
+    @FXML
+    private Button btnRegistrarEmpleados;
+
+    @FXML
+    private TableView<?> tblListaEmpleados;
+
+    @FXML
+    private TextField txtApellidoEmpleado;
+
+    @FXML
+    private TextField txtCedulaEmpleado;
+
+    @FXML
+    private Label txtDireccionEmpleado;
+
+    @FXML
+    private TextField txtEmailEmpleado;
+
+    @FXML
+    private TextField txtNombreEmpleado;
+
+    @FXML
+    void abrirListaEmpleado(ActionEvent event) {
+
+    }
+
+    @FXML
+    void agregarApellidoEmpleado(ActionEvent event) {
+
+    }
+
+    @FXML
+    void agregarCedulaEmpleado(ActionEvent event) {
+
+    }
+
+    @FXML
+    void agregarDireccionEmpleado(ActionEvent event) {
+
+    }
+
+    @FXML
+    void agregarEmailEmpleado(ActionEvent event) {
+
+    }
+
+    @FXML
+    void agregarNombreEmpleado(ActionEvent event) {
+
+    }
+
+    @FXML
+    void generarListaEmpleados(ActionEvent event) {
+
+    }
+
+    @FXML
+    void registroNuevoEmpleado(ActionEvent event) {
+
+    }
+
+}
